05a45cb7162bc3928459aadbc310dd0a478a9102
YASINE LEHMIANI <93382949+yasinelehmiani@users.noreply.github.com>
2023-06-16 11:59:21 +0100

    Update db.js
    update2

--- db/db.js
+++ db/db.js
@@ -17,9 +17,9 @@ export const skills = [
   },
   {
     title: "Design",
-    skillName: "HTML, Bootstrap, Tailwind",
+    skillName: "HTML, Bootstrap, ",
     color: "4",
-    percentage: "70",
+    percentage: "30",
   },
   {
     title: "Version Control",
@@ -28,12 +28,7 @@ export const skills = [
     percentage: "50",
   },
   
-  {
-    title: "Analytics",
-    skillName: "CleverTap, Countly",
-    color: "5",
-    percentage: "50",
-  },
+  
   {
     title: "Editor",
     skillName: "VS Code",
@@ -47,167 +42,85 @@ export const projects = {
     "*** All the projects I listed were completed during my college-level development journey. As for my work at the industry level, those projects are confidential and cannot be disclosed.",
   webProjects: [
     {
-      projectName: "Programming Diaries",
+      projectName: "Retinal Vessel segmentation",
       image: "images/programmingdiaries.png",
       summary:
-        "Developed a full stack blog application to provide content on techical topics across the internet with admin interface.",
-      preview: "https://programmingdiaries.herokuapp.com/",
-      techStack: ["Django", "SQLite", "Bootstrap", "JavaScript", "Heroku"],
+        "We build a segmentation model for retinal vessels using U-Net ,Attention Gate U-Net and Spatial attention U-net.",
+      preview: "https://github.com/8johan/retinalseg",
+      techStack: ["python", "pytorch"],
     },
     {
-      projectName: "Find Your Bank",
+      projectName: "Twitter US Airline Sentiment classification",
       image: "images/findyourbank.png",
       summary:
-        "Developed a React application to render a list of banks fetched from API. Filtered the banks based on queries from localstorage, marked favorites banks.",
-      preview: "https://clever-fermi-0d5d76.netlify.app",
-      techStack: ["Reactjs", "Bootstrap", "JavaScript", "Netlify"],
+        "compare SVM and LSTM for Twitter US Airline Sentiment classification in order to build the best classification model ..",
+      preview: "https://github.com/8johan/deep-learning/blob/main/sentiment_classification1.ipynb",
+      techStack: ["Machine learning", "Deep learning", "keras", "sklearn"],
     },
     {
-      projectName: "Web Portfolio",
+      projectName: "pytorch implementation of U-Net and its variant",
       image: "images/portfolio.png",
       summary:
-        "Web Portfolio to showcase acadmics, skills, projects and contact details in better manner.",
-      preview: "https://github.com/vinaysomawat/Travographer-Portal",
-      techStack: ["HTML", "Bootstrap", "JavaScript"],
+        "implement U-NET ,Attention u-net ,Spatial attention U-Net,U-net++ and Residual U-NET using pytorch.",
+      preview: "https://github.com/8johan/deep-learning/blob/main/U_net_models.ipynb",
+      techStack: ["pytorch", "python"],
     },
-    {
-      projectName: "Resume Builder",
-      image: "images/resume-builder.png",
-      summary:
-        "Browser based editor to build and download Resumes in a customizable templates.",
-      preview: "https://vinaysomawat.github.io/Resume-Builder",
-      techStack: ["HTML", "Bootstrap", "JavaScript"],
-    },
+    
   ],
   softwareProjects: [
     {
-      projectName: "Pizza Ordering ChatBot",
+      projectName: "Testosterone Deficiency classification",
       image: "images/pizzaorderchatbot.png",
       summary:
-        "ChatBot using Dialogflow, Firebase database which stores the chat data in the realtime database.",
-      preview: "https://github.com/vinaysomawat/Pizza-Ordering-ChatBot",
-      techStack: ["Dailogflow", "Firebase"],
+        "build a Testosterone Deficiency classification model using Randomforest.",
+      preview: "https://github.com/8johan/ML-PROJECT/tree/main",
+      techStack: ["python", "sklearn"],
     },
-    {
-      projectName: "WhatsApp-Bot",
-      image: "images/whatsappbot.jpg",
-      summary:
-        "Python script which helps to send messages to WhatsApp contacts automatically using selenium and web automation.",
-      preview: "https://github.com/vinaysomawat/WhatsApp-Bot",
-      techStack: ["Selenium", "Chrome Webdriver", "Python"],
-    },
-    {
-      projectName: "Bill Generator",
-      image: "images/billgenerator.png",
-      summary:
-        "GUI to transfer data to excel sheets and generate bills on the local shops.",
-      preview: "https://github.com/vinaysomawat/Bill-Generator",
-      techStack: ["Tkinter", "Openxlpy", "Python"],
-    },
+    
   ],
   androidProjects: [
-    {
-      projectName: "NITW-CSE",
-      image: "images/nitwcse.jpg",
-      summary:
-        "The Application display details of Department courses, reference books, research, publication and faculty profile.",
-      preview: "https://github.com/vinaysomawat/NITW-CSE",
-      techStack: ["JAVA", "XML", "Android"],
-    },
-    {
-      projectName: "CareerHigh-App",
-      image: "images/carrerhigh.png",
-      summary:
-        "The Application display the webpages of website careerhigh.in in android devices.",
-      preview: "https://github.com/vinaysomawat/CareerHigh-Android",
-      techStack: ["JAVA", "XML", "Android"],
-    },
+    
   ],
   freelanceProjects: [
-    {
-      projectName: "SnylloAir.com",
-      image: "images/snylloair.png",
-      summary:
-        "Developed a company website to showcase the purpose, services and products provided by the company to audience.",
-      preview: "https://www.snylloair.com/",
-      techStack: ["Bootstrap", "JavaScript", "AWS-S3"],
-    },
-    {
-      projectName: "Delivery+",
-      image: "images/AM-Logo-.png",
-      summary: "Android Application to display website in android devices.",
-      preview:
-        "https://play.google.com/store/apps/details?id=com.americanmarket.americanmarketandroid",
-      techStack: ["Android", "JAVA", "Play Store"],
-    },
+    
   ],
 };
 
 export const experience = [
   {
-    title: "Biofourmis India Pvt. Ltd.",
-    duration: "April 2022 - Present",
-    subtitle: "Software Engineer",
+    title: "We are working on a conferance paper.",
+    duration: "/",
+    subtitle: "",
     details: [
-      "Currently involved in developing healthcare and digital therapeutics products."
+      "."
     ],
-    tags: ["JavaScript", "Angular", "Bootstrap", "Nodejs", "Jenkins"],
+    tags: [],
     icon: "heartbeat",
   },
-  {
-    title: "Novopay Solutions Pvt. Ltd.",
-    duration: "June 2020 - April 2022",
-    subtitle: "Software Engineer",
-    details: [
-      "Successfully implemented Aadhaar Enabled Payment services, including Bio-metric eKYC, Cash Withdrawal, Balance Enquiry, Mini-Statements, and Money Transfer, achieving more than 20 story points in each sprint.",
-      "Streamlined the process of loading money into the wallet by integrating QR Code, which reduced the time required by 50%. Also implemented Clevertap events to track user actions, Freshdesk ticketing service, and chat-bot services. Contributed to user onboarding, approval, and finance interfaces.",
-      "Collaborated closely with cross-functional teams such as product, backend, android, and QA, ensuring timely delivery of product builds to meet deadlines",
-    ],
-    tags: ["JavaScript", "Angular", "React", "Bootstrap", "Nodejs", "Jenkins"],
-    icon: "qrcode",
-  },
-  {
-    title: "ThinkPedia LLP",
-    duration: "May 2019 - June 2019",
-    subtitle: "SDE Intern",
-    details: [
-      "Contributed as a full-stack developer to provide technical support to the team",
-      "Designed and developed a comprehensive Web Application for social media management, starting from the ground up, to cater to customer needs.",
-    ],
-    tags: ["JavaScript", "Angular", "Bootstrap", "Java", "Spring Boot"],
-    icon: "group",
-  },
+  
 ];
 
 export const education = [
   {
-    title: "Bachelors in Computer Science and Engineering",
-    duration: "",
-    subtitle: "National Institute of Technology, Warangal",
+    title: "PhD Student in Computer Vision applied in medical images analysis at LIMIARF ",
+    duration: "2023-PRESENT",
+    subtitle: "Faculty of sciences in rabat.UM5",
     details: [
-      "I successfully qualified GATE-2020, demonstrating my aptitude and expertise in the field.",
-      "As an active competitive programmer, I have achieved a CodeChef rating of 1841*, showcasing my skills in solving complex programming problems.",
-      "My GitHub projects have earned over 1k stars and forks, a testament to my ability to create high-quality and impactful code.",
+      ".",
     ],
     tags: [
-      "Data Structures & Algorithms",
-      "Operating Systems",
-      "Database Management System",
-      "Computer Networks",
-      "Compiler Designing",
-      "Cloud Computing",
+      
     ],
     icon: "graduation-cap",
   },
   {
-    title: "Class 11-12th in Science and Mathematics",
+    title: "Master in BioInformatic ",
     duration: "",
-    subtitle: "Board of Secondary Education, Rajasthan",
+    subtitle: "ENSIAS, Rabat",
     details: [
-      "I have successfully qualified JEE Advanced, Main, and BITSAT, demonstrating my strong aptitude and skills in these highly competitive exams.",
-      "Additionally, I am proud to have achieved an exceptional 99.5 percentile in my Class 12th Boards Examinations, reflecting my dedication and hard work towards academic excellence.",
+      ".",
     ],
-    tags: ["Physics", "Chemistry", "Mathematics"],
+    tags: [],
     icon: "book",
   },
 ];
@@ -216,18 +129,12 @@ export const footer = [
   {
     label: "Dev Profiles",
     data: [
-      {
-        text: "Stackoverflow",
-        link: "https://stackoverflow.com/users/8461233/vinay-somawat",
-      },
+     
       {
         text: "GitHub",
-        link: "https://github.com/vinaysomawat",
+        link: "https://github.com/yasinelehmiani",
       },
-      {
-        text: "LeetCode",
-        link: "https://leetcode.com/somawatvinay/",
-      },
+      
     ],
   },
   {
