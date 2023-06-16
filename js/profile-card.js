a1ea8c9f340d9232c41d93eefc7d0cf5669b04ba
YASINE LEHMIANI <93382949+yasinelehmiani@users.noreply.github.com>
2023-06-16 18:39:36 +0100

    Update profile-card.js
    update8

--- js/profile-card.js
+++ js/profile-card.js
@@ -15,7 +15,7 @@ window.addEventListener("DOMContentLoaded", async function () {
         el.innerHTML = `
         <div style="font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji; border-radius: 6px; line-height: 1.5; padding: 16px; font-size: 14px; color: #24292e; background-color: #f9bf3f;">
                 <div style="display: flex; align-items: center;">
-                    <img style="width: 100px; height: 100px; border-radius: 30%" src="${profile_image}" alt="Profile image"></img>
+                    <img style="width: 1000px; height: 1000px; border-radius: 90%" src="${profile_image}" alt="Profile image"></img>
                     <div style="display: flex; flex-direction: column; margin-left: 12px">
                         <span style="font-weight: 500; color: #black; font-size: 18px">
                             <a style="text-decoration: none; color: inherit;" target="_blank" href="${website_url && link}">
